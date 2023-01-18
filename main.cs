729687b9e01cc36969ae9cfe3034bc7a6ae625d6
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2023-01-18 04:00:59 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,41 @@
+// Created by: Ashley Monaghan
+// Created on: Jan 2023
+//
+//This program calculates Pi
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int numberOfLoops;
+        int counter = 0;
+        double denomonaitor = -1;
+        double Pi = 0;
+
+        Console.WriteLine("Calculating the number PI!");
+        Console.WriteLine("");
+
+        Console.WriteLine("Enter the disired number of calculations:");
+        Console.WriteLine("");
+        numberOfLoops = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine(" ");
+
+        while (counter < numberOfLoops) {
+    
+            double numerator = 4;
+            denomonaitor = denomonaitor + 2;
+            double multiply = Math.Pow(-1, counter);
+            double aLittleBit = (numerator / denomonaitor) * multiply;
+            Pi = Pi + aLittleBit;
+            counter = counter + 1;
+        }
+
+        Console.WriteLine("Pie: " + Pi.ToString("0.00") + "!");
+
+        Console.WriteLine("\nDone.");
+    }
+}
